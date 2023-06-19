cff67820662ffc65b1bb3d413e68298be4ebb8f4
Natalia <n.antonova@dats.team>
2023-06-20 01:37:11 +0300

    feat: solve part of streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdin } from 'process';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files/fileToWrite.txt');
+
+    const stream = fs.createWriteStream(filePath);
+    stdin.on('data', (data) => stream.write(data.toString()));
 };
 
 await write();
